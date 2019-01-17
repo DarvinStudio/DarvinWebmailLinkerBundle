a61bc802f025d5e9e8694d57f4a1f12674755565
Igor Nikolaev <igor.sv.n@gmail.com>
2019-01-17 09:22:30 +0300

    .yml => .yaml

--- DependencyInjection/DarvinWebmailLinkerExtension.php
+++ DependencyInjection/DarvinWebmailLinkerExtension.php
@@ -31,6 +31,6 @@ class DarvinWebmailLinkerExtension extends Extension
 //        $config = $this->processConfiguration($configuration, $configs);
 
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('linker.yml');
+        $loader->load('linker.yaml');
     }
 }
